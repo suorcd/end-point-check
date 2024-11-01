5327b93b6f8d4168ff07c7c4cec62eac1a30667d
su || cd <suorcd@fastmail.fm>
2024-10-31 21:21:57 -0700

    more flakes

--- flake.nix
+++ flake.nix
@@ -6,9 +6,8 @@
   };
 
   outputs = { self, nixpkgs }: {
-    packages = let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+    packages.x86_64-linux = let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
     {
       default = pkgs.stdenv.mkDerivation {
